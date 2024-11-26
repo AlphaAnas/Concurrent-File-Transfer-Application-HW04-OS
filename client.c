3e5b1cbe907b690614943a57dbc8f46fd9719c00
AlphaAnas <muhd.anas70pe@gmail.com>
2024-11-26 09:19:50 +0500

    added client and server without tests

--- client.c
+++ client.c
@@ -6,47 +6,114 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <pthread.h>
 
-void error(const char *msg) {
-    perror(msg);
-    exit(0);
+
+typedef struct {
+    int server_socket;
+    FILE *file;
+    int index;
+} ThreadData;
+
+void recieve_segment_data(void *arg){
+    
+    ThreadData *data = (ThreadData *)arg;
+    char buffer[1024];
+    FILE *temp_file; // temp_file is the file where the segment data will be stored
+    char temp_name[32];
+    sprintf(temp_name, "segment_%d.tmp", data->index);
+
+    temp_file = fopen(temp_name, "wb");
+    if (!temp_file) error("Error opening temp file");
+
+    int bytes_received;
+    bytes_received = recv(data->server_socket, buffer, 1024, 0);
+    while (bytes_received > 0) {
+        fwrite(buffer, 1, bytes_received, temp_file);
+        bytes_received = recv(data->server_socket, buffer, 1024, 0);
+    }
+    
+
+    fclose(temp_file);
+    close(data->server_socket);
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    
 
-    //create a socket 
-    int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s <hostname> <port> <file_name> <num_threads>\n", argv[0]);
+        exit(1);
+    }
 
-    //specify an address for the socket
+    int port_no = atoi(argv[2]);
+    int num_threads = atoi(argv[4]);
+    char *file_name = argv[3];
+
+    struct hostent *server = gethostbyname(argv[1]); // get the IP address of the server
+    if (server == NULL) {
+        fprintf(stderr, "Error, no such host\n");
+        exit(0);
+    }
+
+
     struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
-    server_address.sin_addr.s_addr = INADDR_ANY;// shortcut for IP address: 0.0.0.0 
+    for (int i=0; i < num_threads; i++){
+        //create a socket 
+        int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (network_socket < 0) {
+            error("Error opening socket");
+        }
+        //specify an address for the socket
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(port_no);
+        // server_address.sin_addr.s_addr = INADDR_ANY;// shortcut for IP address: 0.0.0.0 
+        // bcopy copies server->h_addr to server_address.sin_addr.s_addr
 
-    int connection_status = connect(network_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+        bcopy((char *)server->h_addr_list[0], (char *)&server_address.sin_addr.s_addr, server->h_length);
 
-    // check for connection
-    if (connection_status == -1) {
-        error("There was an error making a connection to the remote socket\n");
+        // connect to the server
+        int connection_status = connect(network_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+         // check for connection
+        if (connection_status == -1) {
+            error("There was an error making a connection to the remote socket\n");
+        }
+
+        printf("Connected to the server.\n");
+
+        send(network_socket, file_name, strlen(file_name) + 1, 0);// send the file name to the server
+        send(network_socket, &num_threads, sizeof(num_threads), 0); // send the number of threads to the server
+
+        ThreadData *data = malloc(sizeof(ThreadData));
+        data->server_socket = network_socket;
+        data->index = i;
+
+        pthread_t thread;
+        pthread_create(&thread, NULL, recieve_segment_data, data);
+        pthread_detach(thread);
     }
 
-    // kee reading from the server until there is no more data
-    while (1){
+    // now since data is received we need to reassemble all the threads to make a file
+      // Reassemble segments after download
+    FILE *output_file = fopen("output_file", "wb");
+    for (int i = 0; i < num_threads; i++) {
+        char temp_name[32];
+        sprintf(temp_name, "segment_%d.tmp", i);
 
+        FILE *temp_file = fopen(temp_name, "rb");
+        char buffer[1024];
+        int bytes_read;
+        bytes_read = fread(buffer, 1, 1024, temp_file);
+        while (bytes_read > 0) {
+            fwrite(buffer, 1, bytes_read, output_file);
+            bytes_read = fread(buffer, 1, 1024, temp_file);
+        }
 
-    // receive data from the server
-
-    char server_response[256];
-    //recv = receive
-    recv(network_socket, &server_response, sizeof(server_response), 0);
-
-    //print out the server's response
-    printf("The server sent the data: %s\n", server_response);
-    
+        fclose(temp_file);
+        remove(temp_name); // Delete temp file
     }
 
-    //close the socket
-    close(network_socket);
+    fclose(output_file);
+    printf("File transfer complete.\n");
     return 0;
 }
--- server.c
+++ server.c
@@ -5,14 +5,67 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
+
+typedef struct {
+    int client_socket;
+    FILE *file;
+    long start;
+    long end;
+} ThreadData;
+
+void send_segment_data(void *arg){
+
+    ThreadData *data = (ThreadData*) arg;
+    fseek(data->file, data->start, SEEK_SET); // set the file pointer to the start of the segment
+
+    long remaining_bytes = data->end - data->start;
+    char buffer[1024]; // read 1 KB at a time
+    int buffer_size = sizeof(buffer);
+
+
+    while (remaining_bytes > 0){
+        size_t to_read;
+        if (remaining_bytes > buffer_size ){
+             to_read = buffer_size;
+        }
+        else{
+             to_read = remaining_bytes;
+        }
+        fread(buffer, 1, to_read, data->file);
+        send(data->client_socket, buffer, sizeof(buffer), 0);
+        remaining_bytes -= sizeof(buffer);
+        // empty the buffer
+        memset(buffer, 0, sizeof(buffer)); //Set N bytes of S to C. memset(S,C,N)
+
+    }
+
+    fclose(data->client_socket);
+    free(data);
+    
+  
+
+
+
+         
+        // send(client_socket, server_message, sizeof(server_message), 0);
+    
 }
 
 int main(int argc, char *argv[]) {
     
+    
+    
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    int server_socket, port_no;
+
+    port_no = atoi(argv[1]);
+    struct sockaddr_in client_address;
 
     char server_message[256] = "You have reached the server!";
     //create a socket
@@ -30,23 +83,63 @@ int main(int argc, char *argv[]) {
     //bind the socket to our specified IP and port
     bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
-    while (1){
-
     listen(server_socket, 5); // 5 is the number of connections that can be waiting while the server is busy
+    printf("Server is listening on port 8080...\n");
 
-    int client_socket;           
-    client_socket = accept(server_socket, NULL, NULL);
+    while (1) {
+        socklen_t client_len = sizeof(client_address);
+        int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_len);
+        if (client_socket < 0) {
+            error("Error accepting connection");
+        }
 
 
-    //send the message
-    send(client_socket, server_message, sizeof(server_message), 0);
+            // receive the signal from client of the name of the file and no of threads to break into
+        printf("Client connected.\n");
+        char file_name[256];
+        int num_threads;
+        recv(client_socket, file_name, sizeof(file_name), 0);
+        recv(client_socket, &num_threads, sizeof(num_threads), 0);
+        printf("File name: %s\n", file_name);
+        printf("Number of threads: %d\n", num_threads);
+        FILE *file = fopen(file_name, "rb");
+        if (!file) {
+            perror("File not found");
+            close(client_socket);
+            continue;
+        }
 
-    
+        
+        // long segment_size = file_size / num_threads;
 
+
+        //getting the length of the file (https://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c)
+        fseek(file, 0L, SEEK_END); // OL is long int
+        long int size = ftell(file);
+        rewind(file); // go back to the beginning of the file
+        long int segment_size = size / num_threads;
+
+        for (int i = 0; i < num_threads; i++) {
+            ThreadData *data = malloc(sizeof(ThreadData));
+            data->client_socket = client_socket;
+            data->file = file;
+            data->start = i * segment_size;
+            data->end = (i == num_threads - 1) ? size : (i + 1) * segment_size;
+
+            pthread_t thread;
+            pthread_create(&thread, NULL, send_segment_data, data);
+            pthread_detach(thread);
+        }
+
+
+        
+        // send(client_socket, server_message, sizeof(server_message), 0);
+
+        // Close the client socket after sending the message
+        close(client_socket);
     }
 
-    //close the socket
+    
     close(server_socket);
-
     return 0;
 }
